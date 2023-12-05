88f68435e57c97c256baf98fb429818efe801990
Mufidhatul Nafisa <147472717+MufidhatulNafisa035@users.noreply.github.com>
2023-12-05 09:54:48 +0700

    puzzle fidha

--- src/sudoku/Puzzle.java
+++ src/sudoku/Puzzle.java
@@ -1,13 +1,9 @@
 package sudoku;
-/**
- * The Sudoku number puzzle to be solved
- */
+
+import java.util.Random;
+
 public class Puzzle {
-    // All variables have package access
-    // The numbers on the puzzle
-    int[][] numbers = new int[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
-    // The clues - isGiven (no need to guess) or need to guess
-    boolean[][] isGiven = new boolean[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
+    // ... kode lainnya ...
 
     // Constructor
     public Puzzle() {
@@ -18,45 +14,28 @@ public class Puzzle {
     //  to control the difficulty level.
     // This method shall set (or update) the arrays numbers and isGiven
     public void newPuzzle(int cellsToGuess) {
-        // I hardcode a puzzle here for illustration and testing.
-        int[][] hardcodedNumbers =
-                {{5, 3, 4, 6, 7, 8, 9, 1, 2},
-                        {6, 7, 2, 1, 9, 5, 3, 4, 8},
-                        {1, 9, 8, 3, 4, 2, 5, 6, 7},
-                        {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                        {4, 2, 6, 8, 5, 3, 7, 9, 1},
-                        {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                        {9, 6, 1, 5, 3, 7, 2, 8, 4},
-                        {2, 8, 7, 4, 1, 9, 6, 3, 5},
-                        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+        Random random = new Random();
 
-        // Copy from hardcodedNumbers into the array "numbers"
+        // Inisialisasi numbers secara acak
         for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
             for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
-                numbers[row][col] = hardcodedNumbers[row][col];
+                numbers[row][col] = random.nextInt(9) + 1; // angka acak dari 1 hingga 9
             }
         }
 
-        // Need to use input parameter cellsToGuess!
-        // Hardcoded for testing, only 2 cells of "8" is NOT GIVEN
-        boolean[][] hardcodedIsGiven =
-                {{true, true, true, true, true, false, true, true, true},
-                        {true, true, true, true, true, true, true, true, false},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true}};
+        // Inisialisasi isGiven secara acak sesuai dengan jumlah cellsToGuess
+        int cellsGuessed = 0;
+        while (cellsGuessed < cellsToGuess) {
+            int row = random.nextInt(SudokuConstants.GRID_SIZE);
+            int col = random.nextInt(SudokuConstants.GRID_SIZE);
 
-        // Copy from hardcodedIsGiven into array "isGiven"
-        for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
-            for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
-                isGiven[row][col] = hardcodedIsGiven[row][col];
+            // Jika cell belum ditebak sebelumnya
+            if (!isGiven[row][col]) {
+                isGiven[row][col] = true;
+                cellsGuessed++;
             }
         }
     }
 
-    //(For advanced students) use singleton design pattern for this class
+    // ... kode lainnya ...
 }
