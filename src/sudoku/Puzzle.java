5ffb3b829bb0835feefd385ae03f0fb85514160e
Mufidhatul Nafisa <147472717+MufidhatulNafisa035@users.noreply.github.com>
2023-12-07 17:34:17 +0700

    random angka dan kotak fidha

--- src/sudoku/Puzzle.java
+++ src/sudoku/Puzzle.java
@@ -2,42 +2,120 @@ package sudoku;
 /**
  * The Sudoku number puzzle to be solved
  */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 public class Puzzle {
-    // All variables have package access
-    // The numbers on the puzzle
     int[][] numbers = new int[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
-    // The clues - isGiven (no need to guess) or need to guess
     boolean[][] isGiven = new boolean[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
 
-    // Constructor
     public Puzzle() {
         super();
     }
 
-    // Generate a new puzzle given the number of cells to be guessed, which can be used
-    //  to control the difficulty level.
-    // This method shall set (or update) the arrays numbers and isGiven
     public void newPuzzle(int cellsToGuess) {
         // Membuat papan kosong
         for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
             for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
-                numbers[row][col] = 0;  // Set semua nilai di papan sudoku menjadi 0
-                isGiven[row][col] = false;  // Semua kotak tidak diisi
+                numbers[row][col] = 0;
+                isGiven[row][col] = false;
             }
         }
 
-        // Mengisi angka di papan sudoku secara acak dengan aturan sudoku
+        // Mengacak angka di papan sudoku sebelum menyelesaikan puzzle
+        shuffleNumbers();
+
+        // Menyelesaikan puzzle setelah mengacak angka
         solveSudoku();
 
         // Menentukan kotak yang belum terisi (ditebak)
         setGuesses(cellsToGuess);
     }
 
+    private boolean shuffleNumbers() {
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 1; i <= SudokuConstants.GRID_SIZE; i++) {
+            numList.add(i);
+        }
+
+        Collections.shuffle(numList);
+
+        return fillSudoku(numList);
+    }
+
+    private boolean fillSudoku(List<Integer> numList) {
+        return fillSudokuRecursively(0, 0, numList);
+    }
+
+    private boolean fillSudokuRecursively(int row, int col, List<Integer> numList) {
+        if (row == SudokuConstants.GRID_SIZE - 1 && col == SudokuConstants.GRID_SIZE) {
+            return true;
+        }
+
+        if (col == SudokuConstants.GRID_SIZE) {
+            row++;
+            col = 0;
+        }
+
+        if (numbers[row][col] != 0) {
+            return fillSudokuRecursively(row, col + 1, numList);
+        }
+
+        for (int num : numList) {
+            if (isSafe(row, col, num)) {
+                numbers[row][col] = num;
+                if (fillSudokuRecursively(row, col + 1, numList)) {
+                    return true;
+                }
+                numbers[row][col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSafe(int row, int col, int num) {
+        return isSafeRow(row, num) && isSafeCol(col, num) && isSafeSubgrid(row - row % SudokuConstants.SUBGRID_SIZE, col - col % SudokuConstants.SUBGRID_SIZE, num);
+    }
+
+    private boolean isSafeRow(int row, int num) {
+        for (int col = 0; col < SudokuConstants.GRID_SIZE; col++) {
+            if (numbers[row][col] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isSafeCol(int col, int num) {
+        for (int row = 0; row < SudokuConstants.GRID_SIZE; row++) {
+            if (numbers[row][col] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isSafeSubgrid(int rowStart, int colStart, int num) {
+        for (int row = 0; row < SudokuConstants.SUBGRID_SIZE; row++) {
+            for (int col = 0; col < SudokuConstants.SUBGRID_SIZE; col++) {
+                if (numbers[row + rowStart][col + colStart] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    // Metode solveSudoku(), isValidPlacement(), dan setGuesses() tetap sama seperti sebelumnya
+
     private void solveSudoku() {
         solve(0, 0);
     }
 
-    private boolean solve(int row, int col) {
+
+        private boolean solve(int row, int col) {
         if (col == SudokuConstants.GRID_SIZE) {
             col = 0;
             row++;
@@ -82,28 +160,23 @@ public class Puzzle {
     }
 
     private void setGuesses(int cellsToGuess) {
-        // Mengatur angka yang belum muncul sesuai aturan Sudoku
-        int targetFilledCells = cellsToGuess + (SudokuConstants.GRID_SIZE * SudokuConstants.GRID_SIZE) / 2; // Memastikan lebih banyak kotak yang terisi
-        java.util.List<Integer> indexes = new java.util.ArrayList<>();
+        int targetFilledCells = cellsToGuess + (SudokuConstants.GRID_SIZE * SudokuConstants.GRID_SIZE) / 2; // Menargetkan lebih banyak kotak yang terisi
+        List<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < SudokuConstants.GRID_SIZE * SudokuConstants.GRID_SIZE; i++) {
             indexes.add(i);
         }
-        java.util.Collections.shuffle(indexes);
+        Collections.shuffle(indexes);
 
-        int givenCells = 0;
-        for (int i = 0; i < indexes.size() && givenCells < targetFilledCells; i++) {
+        int filledCells = 0;
+        for (int i = 0; i < indexes.size() && filledCells < targetFilledCells; i++) {
             int idx = indexes.get(i);
             int row = idx / SudokuConstants.GRID_SIZE;
             int col = idx % SudokuConstants.GRID_SIZE;
             if (numbers[row][col] != 0) {
                 isGiven[row][col] = true;
-                givenCells++;
+                filledCells++;
             }
         }
     }
 
-
-
-
-    //(For advanced students) use singleton design pattern for this class
 }
