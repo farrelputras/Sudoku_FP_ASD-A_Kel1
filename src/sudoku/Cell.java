5e4449c57c4a70ca883d16b83601a24673bf17dc
Mufidhatul Nafisa <147472717+MufidhatulNafisa035@users.noreply.github.com>
2023-12-08 23:39:44 +0700

    Update Cell.java

--- src/sudoku/Cell.java
+++ src/sudoku/Cell.java
@@ -1,4 +1,15 @@
 package sudoku;
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026221035 - Mufidhatul Nafisa
+ * 2 - 5026221120 - M. Shalahuddin Arif Laksono
+ * 3 - 5026221102 - Fernandio Farrel Putra S.
+ */
+
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JTextField;
